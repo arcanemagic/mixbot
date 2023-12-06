9b08354fb7725243a596c633f87e4f516ad90971
Jess Xu <jsxu315@gmail.com>
2023-12-05 22:36:59 -0800

    Changed form labels to none/less/standard/more

--- homepagehtml.h
+++ homepagehtml.h
@@ -33,6 +33,7 @@ std::string indexhtml = R"(<!DOCTYPE html>
 
         form {
           text-align:center;
+          font-family: Verdana, Geneva, sans-serif;
         }
 
 
@@ -44,57 +45,57 @@ std::string indexhtml = R"(<!DOCTYPE html>
     <h1>Embedded Mixologist</h1>
     <h3>Welcome! Choose a drink:</h3>
 
-    <p>Click <a href="/?drink1=2&drink2=2&drink3=2&drink4=2">here</a> to order drink 1</p>
-    <p>Click <a href="/?drink1=2&drink2=4&drink3=4&drink4=6">here</a> to orderdrink 2</p><br>
+    <p>Click <a href="/?drink1=2&drink2=2&drink3=2&drink4=2">here</a> to order Special #1</p>
+    <p>Click <a href="/?drink1=2&drink2=4&drink3=4&drink4=6">here</a> to order Special #2</p><br>
 
     <h3>Or specify your own:</h3>
 
      <form method="get" action="" enctype="text/plain">
         <p>Drink 1:</p>
         <input type="radio" id="0" name="drink1" value="0" checked="checked">
-        <label for="0">0</label><br>
+        <label for="0">None</label><br>
         <input type="radio" id="2" name="drink1" value="2">
-        <label for="2">2</label><br>
+        <label for="2">Less</label><br>
         <input type="radio" id="4" name="drink1" value="4">
-        <label for="4">4</label><br>
+        <label for="4">Standard</label><br>
         <input type="radio" id="6" name="drink1" value="6">
-        <label for="6">6</label><br>
+        <label for="6">More</label><br>
 
         <br>
 
-        <p>Drink 2</p>
+        <p>Drink 2:</p>
         <input type="radio" id="0" name="drink2" value="0" checked="checked">
-        <label for="0">0</label><br>
+        <label for="0">None</label><br>
         <input type="radio" id="2" name="drink2" value="2">
-        <label for="2">2</label><br>
+        <label for="2">Less</label><br>
         <input type="radio" id="4" name="drink2" value="4">
-        <label for="4">4</label><br>
+        <label for="4">Standard</label><br>
         <input type="radio" id="6" name="drink2" value="6">
-        <label for="6">6</label><br>
+        <label for="6">More</label><br>
 
         <br>
 
         <p>Drink 3:</p>
         <input type="radio" id="0" name="drink3" value="0" checked="checked">
-        <label for="0">0</label><br>
+        <label for="0">None</label><br>
         <input type="radio" id="2" name="drink3" value="2">
-        <label for="2">2</label><br>
+        <label for="2">Less</label><br>
         <input type="radio" id="4" name="drink3" value="4">
-        <label for="4">4</label><br>
+        <label for="4">Standard</label><br>
         <input type="radio" id="6" name="drink3" value="6">
-        <label for="6">6</label><br>
+        <label for="6">More</label><br>
         
         <br>
 
         <p>Drink 4:</p>
         <input type="radio" id="0" name="drink4" value="0" checked="checked">
-        <label for="0">0</label><br>
+        <label for="0">None</label><br>
         <input type="radio" id="2" name="drink4" value="2">
-        <label for="2">2</label><br>
+        <label for="2">Less</label><br>
         <input type="radio" id="4" name="drink4" value="4">
-        <label for="4">4</label><br>
+        <label for="4">Standard</label><br>
         <input type="radio" id="6" name="drink4" value="6">
-        <label for="6">6</label><br>
+        <label for="6">More</label><br>
 
         <br>
 
